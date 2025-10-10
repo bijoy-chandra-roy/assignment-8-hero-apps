1ec18200973ae4d81e55fe0a3410c86d2ef33bc3
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2025-10-10 21:16:17 +0600

    adding styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import hero from "../assets/hero.png";
+import playStore from "../assets/play-store.png";
+import appStore from "../assets/app-store.png";
 
 const Banner = () => {
   return (
     <div>
-      <div className="hero">
+      <div className="hero mt-20">
         <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">
-              We Build{" "}
+          <div className="">
+            <h1 className="text-[72px] font-bold">
+              We Build <br />
               <span className="bg-gradient-to-br from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
                 Productive
               </span>{" "}
               Apps
             </h1>
-            <p className="py-6">
+            <p className="mt-4 py-6y text-[20px] font-normal text-[#627382]">
               At HERO.IO, we craft innovative apps designed to make everyday
               life simpler, smarter, and more exciting.Our goal is to turn your
               ideas into digital experiences that truly make an impact.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <div className="my-10 flex gap-4 justify-center">
+              <button className="btn btn-neutral btn-outline">
+                <img src={playStore} alt="" className="h-8/12" />
+                Google Play
+              </button>
+              <button className="btn btn-neutral btn-outline">
+                <img src={appStore} alt="" className="h-8/12" />
+                App Store
+              </button>
+            </div>
           </div>
         </div>
       </div>
