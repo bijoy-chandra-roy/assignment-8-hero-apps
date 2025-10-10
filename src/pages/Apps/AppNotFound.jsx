dae88cebba3cc4e4c51e95d3e63c7d3a4d546883
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2025-10-10 23:51:43 +0600

    added error pages

--- src/pages/Apps/AppNotFound.jsx
+++ src/pages/Apps/AppNotFound.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Navigate, useNavigate } from "react-router";
+import app404 from "../../assets/App-Error.png";
 
 const AppNotFound = () => {
-    return (
-        <div>
-           <h1>101</h1> 
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img src={app404} alt="" />
+      <div className="text-center flex flex-col gap-2 my-4">
+        <h1 className="text-[48px] font-semibold text-black">
+          OPPS!! APP NOT FOUND
+        </h1>
+        <p className="text-[20px] font-normal text-[#627382]">
+          The App you are requesting is not found on our system. please try
+          another apps
+        </p>
+      </div>
+      <button
+        onClick={() => navigate(-1)}
+        className="btn bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white"
+      >
+        Go Back!
+      </button>
+    </div>
+  );
 };
 
 export default AppNotFound;
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router";
-import error404 from "../assets/error-404.png"
+import error404 from "../assets/error-404.png";
 
 const PageNotFound = () => {
   const navigate = useNavigate();
@@ -8,8 +8,14 @@ const PageNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <img src={error404} alt="" />
-      <h1>Oops, page not found!</h1>
-      <p>The page you are looking for is not available.</p>
+      <div className="text-center flex flex-col gap-2 my-4">
+        <h1 className="text-[48px] font-semibold  text-black">
+          Oops, page not found!
+        </h1>
+        <p className="text-[20px] font-normal text-[#627382]">
+          The page you are looking for is not available.
+        </p>
+      </div>
       <button
         onClick={() => navigate(-1)}
         className="btn bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white"
